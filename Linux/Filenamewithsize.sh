d0d196f5fe79cf82e1cfa301242f2fc86ad5ff57
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2023-06-07 09:46:03 +0200

    Update Filenamewithsize.sh

--- Linux/Filenamewithsize.sh
+++ Linux/Filenamewithsize.sh
@@ -12,3 +12,8 @@ file_info=$(ls -l | awk '{print $9 "," $5}')
 # Print filenames and sizes
 echo "Filename,Size"
 echo "$file_info"
+
+
+#programming exercises can be done with your own Linux booted or permanently installed Linux Mint or with various online bash environments like:
+
+#https://www.jdoodle.com/test-bash-shell-script-online/
