5b72e75c8f3cbc965891eea94c357bdf8286f9fd
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2023-07-10 20:37:50 +0300

    Update AddInfoPersonTable.sql

--- SQL/Chapter1/AddInfoPersonTable.sql
+++ SQL/Chapter1/AddInfoPersonTable.sql
@@ -1,3 +1,12 @@
+
+#Edellisessä tehtävässä laadit tietokantaan taulun "Henkilot". Nyt tehtävänäsi tehdä SQL-lause, jolla saat syötettyä alla olevassa taulukkossa luetellut tiedot juuri laatimaasi tauluun.
+
+#HENKILOT
+#henkilot_henkilo_id	henkilot_etunimi	henkilot_sukunimi	henkilot_email
+#1	Maija	Teräväinen	maija@teravainen.com
+
+
+
 INSERT INTO Henkilot (henkilot_henkilo_id, henkilot_etunimi, henkilot_sukunimi, henkilot_email)
 VALUES (1, 'Maija', 'Teräväinen', 'maija@teravainen.com');
 
