639b2f955052b3ea26b66620105e7a1d000d6b63
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2023-07-12 19:06:53 +0300

    Update TietojenHakeminenKirjat-Taulusta.sql

--- SQL/Chapter2/TietojenHakeminenKirjat-Taulusta.sql
+++ SQL/Chapter2/TietojenHakeminenKirjat-Taulusta.sql
@@ -1,1 +1,6 @@
+#Ellei toisin mainita, luvusta kaksi alkaen kaikki harjoitustehtävät liittyvät kirjatietokantaan, jonka kuvauksen voit lukea manuaalista tai tehtävien yhteydessä olevasta "Harjoitustietokannan kuvaus"-linkistä. Kirjatietokannan tutkiminen on välttämätöntä oikeiden vastausten laatimiseksi harjoitustehtäviin.
 
+#Harjoitustehtävänäsi on laatia SQL-kysely, joka hakee kaikki "Kirjat" taulun sisältämät tiedot.
+
+
+SELECT*FROM Kirjat;
