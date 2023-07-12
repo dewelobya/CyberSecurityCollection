550a83fd9097f10c678fcd019a902ff83848ea03
pe1l1nl1 <19546253+pe1l1nl1@users.noreply.github.com>
2023-07-12 19:20:49 +0300

    Update KirjojenNimienJaHintojenHakeminen.sql

--- SQL/Chapter2/KirjojenNimienJaHintojenHakeminen.sql
+++ SQL/Chapter2/KirjojenNimienJaHintojenHakeminen.sql
@@ -1,4 +1,4 @@
 #Laadi SQL-kysely, jolla haetaan kaikkien kirjatietokannan (Harjoitustietokannan kuvaus) kirjojen nimet ja hinnat. Kysely on laadittava siten, että vastauksessa kullakin rivillä on ensin kirjan nimi ja tämän jälkeen kirjan hinta.
 
-SELECT Nimi, Hinta
-FROM Kirjat;
+SELECT kirjat_kirjannimi, kirjat_hinta
+FROM kirjat;
